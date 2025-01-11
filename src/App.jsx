ca3d2ff65f272ab18ed75151df1108f267e48224
Kayky Renner Siqueira Galindo <kayky.renner.sg@gmail.com>
2025-01-11 10:52:37 -0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import AddTask from './componentes/addTask'
-import Task from './componentes/task'
-function App(){
-  return(
-    <div>
-     <AddTask/>
-     <Task/>
+
+function App() {
+  const [task,setTask] = useState([{
+    id:1,
+    title:'estudar programação',
+  }])
+  return (    
+    <div className="divPrincipalGerenciador">
+      <div className="divGerenciador">
+        <h1 className="gerenciadorDeTarefas" >
+          Gerenciador de Tarefas
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
+
 export default App;
